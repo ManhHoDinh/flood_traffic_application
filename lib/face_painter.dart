34d6bf6e9ee5785435cb47cc146b816ab7df47f0
ManhHoDinh <21522327@gm.uit.edu.vn>
2024-03-04 22:38:43 +0700

    create project

---
+++ lib/face_painter.dart
@@ -0,0 +1,110 @@
+import 'dart:math';
+
+import 'package:climscan/coordinates_translator.dart';
+import 'package:flutter/material.dart';
+import 'dart:ui' as ui;
+import 'package:google_mlkit_face_detection/google_mlkit_face_detection.dart';
+
+class FacePainter extends CustomPainter {
+  ui.Image image;
+  List<Face> faces;
+  List<Rect> rects = [];
+  bool contours;
+  FacePainter(this.image, this.faces, {this.contours = false}) {
+    for (var face in faces) {
+      rects.add(face.boundingBox);
+    }
+  }
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paintBoundingBox = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = contours ? 3.0 : 5.0
+      ..color = Colors.green;
+
+    Paint paintPoint = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 1.5
+      ..color = Colors.white;
+
+    Paint paintLine = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 1.2
+      ..color = Colors.blue;
+
+    canvas.drawImage(image, Offset.zero, Paint());
+
+    for (int i = 0; i < faces.length; i++) {
+      canvas.drawRect(rects[i], paintBoundingBox);
+    }
+    if (!contours) return;
+    for (Face face in faces) {
+      void paintContourPoint(FaceContourType type,
+          {Color color = Colors.blue, bool circle = false}) {
+        final faceContour = face.contours[type];
+        if (faceContour?.points == null) return;
+        for (int i = 0; i < faceContour!.points.length; i++) {
+          Point point1 = faceContour.points[i];
+          Point point2 =
+              faceContour.points[(i + 1) % faceContour.points.length];
+
+          canvas.drawCircle(
+              Offset(
+                translateX(
+                  point1.x.toDouble(),
+                  InputImageRotation.rotation0deg,
+                  size,
+                  Size(
+                    image.width.toDouble(),
+                    image.height.toDouble(),
+                  ),
+                ),
+                translateY(
+                  point1.y.toDouble(),
+                  InputImageRotation.rotation0deg,
+                  size,
+                  Size(
+                    image.width.toDouble(),
+                    image.height.toDouble(),
+                  ),
+                ),
+              ),
+              1,
+              paintPoint);
+          if (i + 1 == faceContour.points.length && circle == false) return;
+          Paint paintLinenew = paintLine;
+          paintLinenew.color = color;
+          canvas.drawLine(Offset(point1.x.toDouble(), point1.y.toDouble()),
+              Offset(point2.x.toDouble(), point2.y.toDouble()), paintLinenew);
+        }
+      }
+
+      paintContourPoint(FaceContourType.face, circle: true);
+      paintContourPoint(FaceContourType.leftEyebrowTop, color: Colors.pink);
+      paintContourPoint(FaceContourType.leftEyebrowBottom, color: Colors.pink);
+      paintContourPoint(FaceContourType.rightEyebrowTop, color: Colors.pink);
+      paintContourPoint(FaceContourType.rightEyebrowBottom, color: Colors.pink);
+      paintContourPoint(FaceContourType.leftEye, color: Colors.red);
+      paintContourPoint(FaceContourType.rightEye, color: Colors.red);
+      paintContourPoint(FaceContourType.upperLipTop,
+          color: Colors.lightBlueAccent);
+      paintContourPoint(FaceContourType.upperLipBottom,
+          color: Colors.lightBlueAccent);
+      paintContourPoint(FaceContourType.lowerLipTop,
+          color: Colors.lightBlueAccent);
+      paintContourPoint(FaceContourType.lowerLipBottom,
+          color: Colors.lightBlueAccent);
+      paintContourPoint(FaceContourType.noseBridge, color: Colors.purple);
+      paintContourPoint(FaceContourType.noseBottom,
+          color: Colors.lightGreenAccent);
+      paintContourPoint(FaceContourType.leftCheek);
+      paintContourPoint(FaceContourType.rightCheek);
+    }
+  }
+
+  @override
+  bool shouldRepaint(FacePainter oldDelegate) {
+    return image != oldDelegate.image || faces != oldDelegate.faces;
+  }
+}
---
+++ lib/screen/detect_screen.dart
@@ -0,0 +1,140 @@
+import 'dart:io';
+import 'dart:typed_data';
+import 'package:climscan/screen/details_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:google_mlkit_face_detection/google_mlkit_face_detection.dart';
+import 'dart:ui' as ui;
+import '../face_painter.dart';
+
+class DetectScreen extends StatefulWidget {
+  const DetectScreen({super.key, this.path = ""});
+  final String path;
+
+  @override
+  State<DetectScreen> createState() => _DetectScreenState();
+}
+
+class _DetectScreenState extends State<DetectScreen> {
+  List<Face>? faces;
+  InputImage? inputImage;
+  ui.Image? image;
+  bool onDetail = false;
+  bool contours = false;
+
+  @override
+  void initState() {
+    inputImage = InputImage.fromFilePath(widget.path);
+    detectFaces();
+    super.initState();
+  }
+
+  void detectFaces() async {
+    loadImage(widget.path);
+    final faceDetector = FaceDetector(
+      options: FaceDetectorOptions(
+        enableClassification: true,
+        enableContours: true,
+        enableLandmarks: true,
+        enableTracking: true,
+        performanceMode: FaceDetectorMode.accurate,
+      ),
+    );
+    var detectFaces = await faceDetector.processImage(inputImage!);
+    setState(() {
+      faces = detectFaces;
+    });
+  }
+
+  void loadImage(String path) async {
+    final Uint8List data = await File(path).readAsBytes();
+    await decodeImageFromList(data).then((value) {
+      setState(() {
+        image = value;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text('Face Detector'),
+        ),
+        body: (image == null || faces == null)
+            ? const Center(
+                child: Text(
+                  "Processing...",
+                  style: TextStyle(
+                    fontSize: 25,
+                  ),
+                ),
+              )
+            : onDetail
+                ? FaceDetectorDetail(
+                    faces: faces!,
+                  )
+                : Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Center(
+                          child: Container(
+                            margin: const EdgeInsets.only(top: 15),
+                            child: ConstrainedBox(
+                              constraints: BoxConstraints(
+                                maxHeight:
+                                    MediaQuery.of(context).size.height / 1.5,
+                              ),
+                              child: FittedBox(
+                                child: SizedBox(
+                                  width: image!.width.toDouble(),
+                                  height: image!.height.toDouble(),
+                                  child: CustomPaint(
+                                    painter: FacePainter(image!, faces!,
+                                        contours: contours),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Center(
+                          child: Container(
+                            margin: const EdgeInsets.symmetric(vertical: 12),
+                            child: Text(
+                              "Detected: ${faces!.length} person${faces!.length > 1 ? 's' : ''}",
+                            ),
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            ElevatedButton(
+                              onPressed: () {
+                                setState(() {
+                                  onDetail = !onDetail;
+                                });
+                              },
+                              child: Text('DETAILS'),
+                            ),
+                            ElevatedButton(
+                              onPressed: () {
+                                setState(() {
+                                  contours = !contours;
+                                });
+                              },
+                              child: Text('CONTOURS'),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+      ),
+    );
+  }
+}
